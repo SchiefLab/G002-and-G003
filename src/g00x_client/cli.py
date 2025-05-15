36b4e37b2797f5a7a6defe4a5e7fd9491a30a9df
Caleb Kibet <ckibet@iavi.org>
2025-05-15 16:24:56 +0300

    adding g002 and g003 pipeline

--- src/g00x_client/cli.py
+++ src/g00x_client/cli.py
@@ -1,5 +1,6 @@
 import click
-
+from g00x.app import g002
+from g00x.app import g003
 from g00x_figures.cli import figures
 from VISC_codebase.cli import comparison_tables
 
@@ -17,7 +18,8 @@ figures.add_command(comparison_tables, name="comparison-tables")
 # Main Figures
 main.add_command(figures, name="plot")
 # Pipeline
-# main.add_command(pipeline, name="pipeline")
+main.add_command(g002, name="g002")
+main.add_command(g003, name="g003")
 
 if __name__ == "__main__":
     # Run the CLI
